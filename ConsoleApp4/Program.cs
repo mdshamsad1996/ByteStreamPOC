0723964c277d6d56f73048b22a6668ff69edb1b4
shamsadgrokr <shamsad.alam@datagrokr.com>
2019-12-03 23:57:23 +0530

    WIP: Adding ByteStream

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ReadFileName = @"C:\Users\91943\Desktop\MyLearning\BYTE_STREAM_POC\Songs\Move On - Inder Chahal.mp3";
+
+            string CreatingFilePath = @"C:\Users\91943\Desktop\MyLearning\BYTE_STREAM_POC\Songs\copiedSongs.mp3";
+            
+
+            
+
+            //Delete if Already Existing file
+            if(File.Exists(CreatingFilePath))
+                File.Delete(CreatingFilePath);
+
+            FileStream fileStreamForReading = new FileStream(ReadFileName, FileMode.Open, FileAccess.Read);
+
+            FileStream fileStream = new FileStream(CreatingFilePath, FileMode.Create);
+
+            BinaryReader br = new BinaryReader(fileStreamForReading);
+            while (true) {
+    
+                byte[] b = br.ReadBytes(1000);
+
+                Console.WriteLine(b.Length);
+
+                if (b.Length == 0)
+                    break;
+
+                for (int i = 0; i < b.Length; i++)
+                {
+                    fileStream.WriteByte(b[i]);
+                }
+            }
+           // Console.WriteLine(bytes.Length);
+            Console.ReadLine();
+
+
+        }
+    }
+}
